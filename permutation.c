c87a110f010019d536c0c2964a6b37bb0169b277
jdvillamin <jdvillamin@up.edu.ph>
2024-11-28 10:33:44 +0800

    docs: edit documentation

--- permutation.c
+++ permutation.c
@@ -1,38 +1,36 @@
 /*
- * Author: Jan Neal Isaac D. Villamin
- * Lab section: T-2L
- * Date created: 2024-10-22
- * Program description:
- * 	A program that generates all permutations of integers from 1 to N.
- * Reference: https://github.com/ajjacildo/lfs/blob/main/lfs.c
- */
+    Date created: 2024-10-22
+    Program description:
+        A program that generates all permutations of integers from 1 to N.
+    Reference: https://github.com/ajjacildo/lfs/blob/main/lfs.c
+*/
 
 #include <stdio.h>
 
 int main() {
-	int N;
-	printf("Enter N: ");
-	scanf("%d", &N);
-	int nopts[N + 2], option[N + 2][N + 2], start, move, i, candidate;
-	move = start = 0;
-	nopts[start] = 1;
-	while (nopts[start] > 0) {
-		if (nopts[move] > 0) {
-			nopts[++move] = 0;
-			if (move > N)
-				for (i = 1; i < move; i++) {
-					printf("%d", option[i][nopts[i]]);
-					printf(i < move - 1 ? " " : "\n");
-				}
-			for (candidate = N; candidate >= 1; candidate--) {
-				for (i = move - 1; i >= 1; i--)
-					if (option[i][nopts[i]] == candidate)
-						break;
-				if (i == 0)
-					option[move][++nopts[move]] = candidate;
-			}
-		} else 
-			nopts[--move]--;
-	}
-	return 0;
+    int N;
+    printf("N: ");
+    scanf("%d", &N);
+    int nopts[N + 2], option[N + 2][N + 2], start, move, i, candidate;
+    move = start = 0;
+    nopts[start] = 1;
+    while (nopts[start] > 0) {
+        if (nopts[move] > 0) {
+            nopts[++move] = 0;
+            if (move > N)
+                for (i = 1; i < move; i++) {
+                    printf("%d", option[i][nopts[i]]);
+                    printf(i < move - 1 ? " " : "\n");
+                }
+            for (candidate = N; candidate >= 1; candidate--) {
+                for (i = move - 1; i >= 1; i--)
+                    if (option[i][nopts[i]] == candidate)
+                        break;
+                if (i == 0)
+                    option[move][++nopts[move]] = candidate;
+            }
+        } else
+            nopts[--move]--;
+    }
+    return 0;
 }
--- subset_sum.c
+++ subset_sum.c
@@ -1,5 +1,13 @@
+/*
+    Date created: 2024-11-28
+    Program description:
+        A program that finds all subsets of an array of integers that sum to a particular target sum.
+*/
+
 #include <stdio.h>
 
+// a function that takes in an array of integers of length N and a target sum
+// it prints the distinct subsets that sum to the target and returns the number of such subsets
 int subset_sum(int a[], int N, int target) {
     int nopts[N + 2], option[N + 2][N + 2], start, move, i, j, candidate, found = 0;
     move = start = 0;
@@ -11,21 +19,28 @@ int subset_sum(int a[], int N, int target) {
                 if (move == 1 || candidate > option[move - 1][nopts[move - 1]])
                     option[move][++nopts[move]] = candidate;
         } else {
+            // since before a backtrack, we get a unique combination
+            // we use the combination as indices to generate a subset
+            // get the sum
             int sum = 0;
             for (i = 1; i < move; i++)
                 sum += a[option[i][nopts[i]] - 1];
+            // then check if the sum equals the target sum
             if (sum == target) {
                 printf("%d = ", target);
+                // print the subset in terms of their indices
                 for (int i = 1; i < move; i++) {
                     int index = option[i][nopts[i]] - 1;
                     printf("a[%d]", index);
                     printf(i < move - 1 ? " + " : " = ");
                 }
+                // print the subset in terms of their actual values
                 for (int i = 1; i < move; i++) {
                     int index = option[i][nopts[i]] - 1;
                     printf("%d", a[index]);
                     printf(i < move - 1 ? " + " : "\n");
                 }
+                // increase the number of subsets found
                 found++;
             }
             nopts[--move]--;
