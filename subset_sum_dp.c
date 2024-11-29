020782f5c65bfa854d5019ae001c14a1f62e7f08
jdvillamin <jdvillamin@up.edu.ph>
2024-11-29 15:31:26 +0800

    feat: added retrieval of subset

--- subset_sum_dp.c
+++ subset_sum_dp.c
@@ -6,20 +6,43 @@
 
 #include <stdio.h>
 
-#define MAX_SUM 1000
-
 // a function that takes in an array of integers of length N and a target sum
-int subset_sum(int a[], int N, int target) {
-    int dp[2][MAX_SUM + 1];
-    for (int i = 0; i < 2; i++)
-        for (int j = 0; j <= MAX_SUM; j++)
-            dp[i & 1][j] = 0;
+int subset_sum(int a[], int N, int target) { 
+    int max_sum = 0, i, j;
+    for (i = 0; i < N; i++)
+        max_sum += a[i];
+    int dp[N + 1][max_sum + 1], previous[max_sum + 1];
+    for (i = 0; i <= N; i++)
+        for (j = 0; j <= max_sum; j++)
+            dp[i][j] = 0;
     dp[0][0] = 1;
-    for (int i = 0; i < N; i++)
-        for (int j = 0; j <= MAX_SUM; j++)
-            // either we don't take a[i] or we take a[i]
-            dp[i & 1 ^ 1][j] = dp[i & 1][j] | (j >= a[i] ? dp[i & 1][j - a[i]] : 0);
-    return dp[N & 1][target];
+    for (i = 0; i < N; i++)
+        for (j = 0; j <= max_sum; j++) {
+            // first case is don't take a[i]
+            dp[i + 1][j] = dp[i][j];
+            // second case is take a[i]
+            if (j >= a[i]) {
+                dp[i + 1][j] |= dp[i][j - a[i]];
+                // set the last element that makes the sum j to be a[i]
+                previous[j] = a[i];
+            }
+        }
+    // retrieve the subset the sums to the target
+    if (dp[N][target]) {
+        printf("%d = ", target);
+        int current_sum = target;
+        while (current_sum > 0) {
+            printf("%d", previous[current_sum]);
+            // subtract the last term and recurse to the subtracted sum
+            current_sum -= previous[current_sum];
+            if (current_sum == 0)
+                break;
+            printf(" + ");
+        }
+        printf("\n");
+        return 1;
+    }
+    return 0;
 }
     
 int main() {
@@ -33,6 +56,7 @@ int main() {
     int target;
     printf("Target sum: ");
     scanf("%d", &target);
+    printf("Result:\n");
     printf("Subset sum exists: %d\n", subset_sum(a, N, target));
     return 0;
 }
