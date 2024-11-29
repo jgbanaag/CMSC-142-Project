4664824436ad4c7d7730784dad90816987ed81b5
jdvillamin <jdvillamin@up.edu.ph>
2024-11-29 15:33:29 +0800

    feat: added space optimization

--- subset_sum_dp.c
+++ subset_sum_dp.c
@@ -11,38 +11,37 @@ int subset_sum(int a[], int N, int target) {
     int max_sum = 0, i, j;
     for (i = 0; i < N; i++)
         max_sum += a[i];
-    int dp[N + 1][max_sum + 1], previous[max_sum + 1];
+    int dp[2][max_sum + 1], previous[max_sum + 1];
     for (i = 0; i <= N; i++)
         for (j = 0; j <= max_sum; j++)
-            dp[i][j] = 0;
+            dp[i & 1][j] = 0;
     dp[0][0] = 1;
     for (i = 0; i < N; i++)
         for (j = 0; j <= max_sum; j++) {
             // first case is don't take a[i]
-            dp[i + 1][j] = dp[i][j];
+            dp[i & 1 ^ 1][j] = dp[i & 1][j];
             // second case is take a[i]
             if (j >= a[i]) {
-                dp[i + 1][j] |= dp[i][j - a[i]];
+                dp[i & 1 ^ 1][j] |= dp[i & 1][j - a[i]];
                 // set the last element that makes the sum j to be a[i]
                 previous[j] = a[i];
             }
         }
     // retrieve the subset the sums to the target
-    if (dp[N][target]) {
-        printf("%d = ", target);
-        int current_sum = target;
-        while (current_sum > 0) {
-            printf("%d", previous[current_sum]);
-            // subtract the last term and recurse to the subtracted sum
-            current_sum -= previous[current_sum];
-            if (current_sum == 0)
-                break;
-            printf(" + ");
-        }
-        printf("\n");
-        return 1;
+    if (!dp[N & 1][target])
+        return 0;
+    printf("%d = ", target);
+    int current_sum = target;
+    while (current_sum > 0) {
+        printf("%d", previous[current_sum]);
+        // subtract the last term and recurse to the subtracted sum
+        current_sum -= previous[current_sum];
+        if (current_sum == 0)
+            break;
+        printf(" + ");
     }
-    return 0;
+    printf("\n");
+    return 1;
 }
     
 int main() {
