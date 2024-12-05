a36e5daa6f69845a338974a130acd740e83e0892
jdvillamin <jdvillamin@up.edu.ph>
2024-12-05 21:32:07 +0800

    feat: updated milestone 3

--- milestone3.c
+++ milestone3.c
@@ -6,72 +6,77 @@
 
 #include <stdio.h>
 
+#define INF 0xFFFFFFFF
+
 // a function that takes in an array of integers of length N and a target sum
 // it prints the distinct subsets that sum to the target and returns the number of such subsets
-int subset_sum(int a[], int N, int target, int b[]) {
-    // remove a[i] if b[i] = 1
-    int removed_count = 0, i, target0 = target;
-    for (i = 0; i < N; i++) {
-        removed_count += b[i];
-        target -= b[i] * a[i];
-    }
-    printf("%d\n", removed_count);
-    int new_a[N - removed_count], real_index[N - removed_count], count = 0;
-    for (i = 0; i < N; i++)
-        if (!b[i]) {
-            new_a[count] = a[i];
-            real_index[count++] = i;
+int subset_sum(int a[], int N, int b[], int M, int target) {
+    // remove elements from a that are in b by marking them as INFINITY
+    int c[N - M], real_index_initial[N], real_index_remaining[N - M], initial_sum = 0, i, j, k;
+    for (i = 0; i < M; i++) {
+        for (j = 0; j < N; j++) {
+            if (a[j] == b[i]) {
+                a[j] = INF;
+                initial_sum += b[i];
+                real_index_initial[i] = j;
+            }
         }
-    int N0 = N;
-    N -= removed_count;
-    int nopts[N + 2], option[N + 2][N + 2], start, move, j, candidate, found = 0;
+    }
+    // build the new array c that contains the remaining elements and also store their real indices
+    for (i = 0, k = 0; i < N && k < N - M; i++) {
+        if (a[i] != INF) {
+            c[k] = a[i];
+            real_index_remaining[k++] = i;
+        }
+    }
+    int nopts[N - M + 2], option[N - M + 2][N - M + 2], start, move, candidate, found = 0;
     move = start = 0;
     nopts[start] = 1;
     while (nopts[start] > 0) {
         if (nopts[move] > 0) {
             nopts[++move] = 0;
-            for (candidate = N; candidate >= 1; candidate--)
+            for (candidate = N - M; candidate >= 1; candidate--)
                 if (move == 1 || candidate > option[move - 1][nopts[move - 1]])
                     option[move][++nopts[move]] = candidate;
         } else {
-            // since before a backtrack, we get a unique combination
-            // we use the combination as indices to generate a subset
-            // get the sum
             int sum = 0;
             for (i = 1; i < move; i++)
-                sum += new_a[option[i][nopts[i]] - 1];
-            // then check if the sum equals the target sum
-            if (sum == target) {
-                printf("%d = ", target0);
-                for (i = 0; i < N0; i++)
-                    if (b[i]) {
-                        if (i > 0)
-                            printf(" + ");
-                        printf("a[%d]", i);
-                    }
-                // print the subset in terms of their indices
-                for (i = 1; i < move; i++) {
-                    if (removed_count && i == 1)
+                sum += c[option[i][nopts[i]] - 1];
+            if (initial_sum + sum == target) {
+                printf("%d = (", target);
+                for (i = 0; i < M; i++) {
+                    if (i > 0)
                         printf(" + ");
-                    int index = real_index[option[i][nopts[i]] - 1];
-                    printf("a[%d]", index);
-                    printf(i < move - 1 ? " + " : " = ");
+                    printf("%d", b[i]);
                 }
-                for (i = 0; i < N0; i++)
-                    if (b[i]) {
-                        if (i > 0)
-                            printf(" + ");
-                        printf("%d", a[i]);
-                    }
-                // print the subset in terms of their actual values
+                printf(")");
                 for (i = 1; i < move; i++) {
-                    if (removed_count && i == 1)
+                    if (i == 1)
+                        printf(" + (");
+                    if (i > 1)
                         printf(" + ");
-                    int index = option[i][nopts[i]] - 1;
-                    printf("%d", new_a[index]);
-                    printf(i < move - 1 ? " + " : "\n");
+                    printf("%d", c[option[i][nopts[i]] - 1]);
+                    if (i == move - 1)
+                        printf(")");
                 }
-                // increase the number of subsets found
+                printf(" = (");
+                for (i = 0; i < M; i++) {
+                    if (i > 0)
+                        printf(" + ");
+                    printf("a[%d]", real_index_initial[i]);
+                }
+                printf(")");
+                for (i = 1; i < move; i++) {
+                    if (i == 1)
+                        printf(" + (");
+                    if (i > 1)
+                        printf(" + ");
+                    printf("a[%d]", real_index_remaining[option[i][nopts[i]] - 1]);
+                    if (i == move - 1)
+                        printf(")\n");
+                }
+                printf("\n");
+
                 found++;
             }
             nopts[--move]--;
@@ -88,14 +93,18 @@ int main() {
     int a[N], i;
     for (i = 0; i < N; i++)
         scanf("%d", &a[i]);
-    printf("Picked: ");
-    int b[N];
-    for (i = 0; i < N; i++)
+    printf("M: ");
+    int M;
+    scanf("%d", &M);
+    printf("Initial set: ");
+    int b[M];
+    for (i = 0; i < M; i++)
         scanf("%d", &b[i]);
     int target;
     printf("Target sum: ");
     scanf("%d", &target);
     printf("Result:\n");
-    printf("Number of subsets found: %d\n", subset_sum(a, N, target, b));
+    printf("Formula: <target> = (<initial sum>) + (<solution sum>)\n\n");
+    printf("Number of subsets found: %d\n", subset_sum(a, N, b, M, target));
     return 0;
 }
